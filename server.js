362128aee2c68d27aba872751851acc4eb790733
SangHuPark <tkdgn5408407@gmail.com>
2022-08-16 15:48:10 +0900

    Add authMailRouter

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ dotenv.config();
 const loginRouter = require('./routes/loginRouter.js');
 const enrollRouter = require('./routes/enrollRouter.js');
 const resignRouter = require('./routes/resignRouter.js');
-const duplicateRouter = require('./routes/duplicateRouter');
+const duplicateRouter = require('./routes/duplicateRouter.js');
+const authMailRouter = require('./routes/authMailRouter.js');
 
 const app = express();
 app.set('port', process.env.PORT || 3003);
@@ -18,6 +19,7 @@ app.use('/login', loginRouter)
 app.use('/enroll', enrollRouter);
 app.use('/resign', resignRouter);
 app.use('/duplicateCheck', duplicateRouter);
+app.use('/authMail', authMailRouter);
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), "port connected!!");
