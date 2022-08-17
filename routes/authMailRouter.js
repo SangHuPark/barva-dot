dd4edca087a46d6308752d072b9779cc02d5ebe3
SangHuPark <tkdgn5408407@gmail.com>
2022-08-17 14:45:14 +0900

    Folder Organize

--- routes/authMailRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const userController = require('../controllers/userController.js');
-
-const router = express.Router();
-
-router.route('/')
-    .post(userController.sendMail);
-
-module.exports = router;
