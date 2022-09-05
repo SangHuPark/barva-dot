7b2695c49ed11ef0efa530e3c578058cc2a81374
SangHuPark <tkdgn5408407@gmail.com>
2022-09-05 21:55:50 +0900

    Image Refactoring

--- imageTest.js
+++ imageTest.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const multer = require('multer');
-const multerGoogleStorage = require('multer-google-storage');
+const path = require('path');
+const fs = require('fs');
+
+try {
+  fs.readdirSync("upload");
+} catch (err) {
+  console.error("Not Exist 'upload' Folder. Creating Folder...");
+  fs.mkdirSync("upload");
+}
+
+const fileStorage = multer.diskStorage({
+  // 저장 폴더 위치
+  destination: (req, file, cb) => {
+    cb(null, "upload/");
+  },
+  //파일이름
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-barva-${file.originalname}`);
+  },
+});
+
+const multerUpload = multer({
+  storage: fileStorage,
+  limits: {
+    fileSize: 20 * 1080 * 1080, // 20MB 로 제한
+  },
+});
+
+module.exports = multerUpload;
 
 const router = express.Router();
-
-const upload = multer({
-    storage: multerGoogleStorage.storageEngine({
-        bucket: 'barva-dot',
-        projectId: 'vocal-clone-361507',
-        keyFilename: 'vocal-clone-361507-0ca04cc3f265.json',
-    }),
-    /**
-    * limits: { fileSize: 1080 * 1080 },
-    */
-});
 
 router.route('/')
     .post(upload.single('img'), (req, res) => {
