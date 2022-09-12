8450c707029cc8a5caef67f5b8803bac93efa68e
SangHuPark <tkdgn5408407@gmail.com>
2022-09-12 15:48:50 +0900

    Form Apply

--- API/validation/form.js
+++ API/validation/form.js
@@ -2,14 +2,15 @@ const Joi = require('joi');
 const util = require('../function/replyFunc.js');
 
 exports.enrollCheck = async (req, res, next) => {  
-    const body = req.body; 
+    var { user_name, user_nick, user_id, user_pw, user_confirmPw, user_email, marketing } = req.body;
+    var body =  { user_name, user_nick, user_id, user_pw, user_confirmPw, user_email, marketing }
 
-    const user_name_pattern = /^[가-힣|a-z|A-Z]+$/;
-    const user_nick_pattern = /^[가-힣|a-z|A-Z|0-9]+$/;
-    const user_id_pattern = /^[a-z|A-Z|0-9]+$/;
-    const user_pw_pattern = /^[a-z|A-Z|0-9|~!@#$%^&*()_+|<>?:{}]+$/;
+    var user_name_pattern = /^[가-힣|a-z|A-Z]+$/;
+    var user_nick_pattern = /^[가-힣|a-z|A-Z|0-9]+$/;
+    var user_id_pattern = /^[a-z|A-Z|0-9]+$/;
+    var user_pw_pattern = /^[a-z|A-Z|0-9|~!@#$%^&*()_+|<>?:{}]+$/;
     
-    const enrollSchema = Joi.object().keys({ 
+    var enrollSchema = Joi.object().keys({ 
         user_name: Joi.string()
             .min(2)
             .max(10)
@@ -45,16 +46,16 @@ exports.enrollCheck = async (req, res, next) => {
 
         console.log(err);
 
-    	return res.json(util.dataReply(dataReply, false, 400, "요청한 데이터 형식이 올바르지 않습니다.", { err: err.message }));
+    	return res.json(util.dataReply(dataReply, false, 300, "요청한 데이터 형식이 올바르지 않습니다.", { err: err.message }));
     }
 }
 
 exports.idCheck = async (req, res, next) => {
-    const user_id = req.body;
+    var user_id = req.body;
 
-    const user_id_pattern = /^[a-z|A-Z|0-9]+$/;
+    var user_id_pattern = /^[a-z|A-Z|0-9]+$/;
 
-    const idSchema = Joi.object().keys({
+    var idSchema = Joi.object().keys({
         user_id: Joi.string()
             .min(5)
             .max(15)
@@ -71,16 +72,16 @@ exports.idCheck = async (req, res, next) => {
 
         console.log(err);
 
-        return res.json(util.dataReply(dataReply, false, 401, "아이디 형식이 올바르지 않습니다.", { err: err.message }));
+        return res.json(util.dataReply(dataReply, false, 301, "아이디 형식이 올바르지 않습니다.", { err: err.message }));
     }
 }
 
 exports.nickCheck = async (req, res, next) => {
-    const user_nick = req.body.user_nick;
+    var user_nick = req.body.user_nick;
 
-    const user_nick_pattern = /^[a-z|A-Z|0-9|~!@#$%^&*()_+|<>?:{}]+$/;
+    var user_nick_pattern = /^[a-z|A-Z|0-9|~!@#$%^&*()_+|<>?:{}]+$/;
 
-    const nickSchema = Joi.object().keys({
+    var nickSchema = Joi.object().keys({
         user_nick: Joi.string()
             .min(2)
             .max(15)
@@ -97,14 +98,14 @@ exports.nickCheck = async (req, res, next) => {
 
         console.log(err);
 
-        return res.json(util.dataReply(dataReply, false, 402, "닉네임 형식이 올바르지 않습니다.", { err: err.message }));
+        return res.json(util.dataReply(dataReply, false, 303, "닉네임 형식이 올바르지 않습니다.", { err: err.message }));
     }
 }
 
 exports.emailCheck = async (req, res, next) => {
-    const user_email = req.body.user_email;
+    var user_email = req.body;
 
-    const emailSchema = Joi.object().keys({
+    var emailSchema = Joi.object().keys({
         user_email: Joi.string().email().required(),
     })
 
@@ -117,6 +118,34 @@ exports.emailCheck = async (req, res, next) => {
 
         console.log(err);
 
-        return res.json(util.dataReply(dataReply, false, 403, "이메일 형식이 올바르지 않습니다.", { err: err.message }));
+        return res.json(util.dataReply(dataReply, false, 305, "이메일 형식이 올바르지 않습니다.", { err: err.message }));
+    }
+}
+
+exports.pwCheck = async (req, res, next) => {
+    var { user_pw, user_confirmPw } = req.body;
+    var newPw = { user_pw, user_confirmPw };
+
+    var user_pw_pattern = /^[a-z|A-Z|0-9|~!@#$%^&*()_+|<>?:{}]+$/;
+
+    var pwSchema = Joi.object().keys({
+        user_pw: Joi.string()
+            .min(6)
+            .max(15)
+            .pattern(new RegExp(user_pw_pattern))
+            .required(), 
+        user_confirmPw: Joi.string().valid(Joi.in('user_pw')),
+    })
+
+    try {
+        await pwSchema.validateAsync(newPw);
+
+        return next();
+    } catch(err) {
+        var dataReply = {};
+
+        console.log(err);
+
+        return res.json(util.dataReply(dataReply, false, 312, "비밀번호를 확인하세요.", { err: err.message }));
     }
 }
