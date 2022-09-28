1572883bab577d3130c1bc9494b3197b524e8cc7
SangHuPark <tkdgn5408407@gmail.com>
2022-09-28 20:15:56 +0900

    change Router path & userProfile Router apply

--- server.js
+++ server.js
@@ -3,17 +3,21 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-var loginRouter = require('./API/routes/loginRouter.js');
-var enrollRouter = require('./API/routes/enrollRouter.js');
-var duplicatedIdRouter = require('./API/routes/duplicatedIdRouter.js');
-var duplicatedNickRouter = require('./API/routes/duplicatedNickRouter.js');
-var authMailRouter = require('./API/routes/authMailRouter.js');
-var inspectMailRouter = require('./API/routes/inspectMailRouter.js');
-var findIdRouter = require('./API/routes/findIdRouter.js');
-var findPwRouter = require('./API/routes/findPwRouter.js');
-var findPwMailRouter = require('./API/routes/findPwMailRouter.js');
-var updatePwRouter = require('./API/routes/updatePwRouter.js');
-var resignRouter = require('./API/routes/resignRouter.js');
+// 회원 관련 라우터
+var loginRouter = require('./API/routes/userRouter/loginRouter.js');
+var enrollRouter = require('./API/routes/userRouter/enrollRouter.js');
+var duplicatedIdRouter = require('./API/routes/userRouter/duplicatedIdRouter.js');
+var duplicatedNickRouter = require('./API/routes/userRouter/duplicatedNickRouter.js');
+var authMailRouter = require('./API/routes/userRouter/authMailRouter.js');
+var inspectMailRouter = require('./API/routes/userRouter/inspectMailRouter.js');
+var findIdRouter = require('./API/routes/userRouter/findIdRouter.js');
+var findPwRouter = require('./API/routes/userRouter/findPwRouter.js');
+var findPwMailRouter = require('./API/routes/userRouter/findPwMailRouter.js');
+var updatePwRouter = require('./API/routes/userRouter/updatePwRouter.js');
+var resignRouter = require('./API/routes/userRouter/resignRouter.js');
+
+// 홈 관련 라우터
+var profileRouter = require('./API/routes/homeRouter/userProfileRouter.js');
 
 const app = express();
 
@@ -22,6 +26,7 @@ app.set('port', process.env.PORT || 3003);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// 회원 관련 경로
 app.use('/login', loginRouter);
 app.use('/enroll', enrollRouter);
 app.use('/duplicatedIdCheck', duplicatedIdRouter);
@@ -33,6 +38,9 @@ app.use('/findPw', findPwRouter);
 app.use('/findPwMail', findPwMailRouter);
 app.use('/updatePw', updatePwRouter);
 app.use('/resign', resignRouter);
+
+// 홈 관련 경로
+app.use('/userProfile', profileRouter);
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), "port connected!!");
