12a323763ad22bbe70841b8f23fdce28f8ad02fe
SangHuPark <tkdgn5408407@gmail.com>
2022-09-28 18:43:08 +0900

    authMail Router form Apply

--- API/routes/authMailRouter.js
+++ API/routes/authMailRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const userController = require('../controllers/userController.js');
+const form = require('../validation/form.js');
 
 const router = express.Router();
 
 router.route('/')
-    .post(userController.sendMail);
+    .post(form.emailForm, userController.sendMail);
 
 module.exports = router;
