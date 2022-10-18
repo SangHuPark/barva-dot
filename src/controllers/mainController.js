8db380f7ece8fceb34982d2614ca744c8870f436
SangHuPark <tkdgn5408407@gmail.com>
2022-10-18 17:59:11 +0900

    checkerboard, single API apply

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -93,12 +93,18 @@ export async function uploadPost(req, res) {
 // 바둑판 형식으로 사용자 게시물 불러오기
 export async function userCheckerboard(req, res) {
     const user_id = req.decoded.user_id;
-    const post_id = req.body.post_id;
 
     try {
-        const loadingResult = await mainService.importPost(user_id, post_id);
+        const checkerboardResult = await mainService.importUserCheckerboard(user_id);
+        const emptyArr = [];
+        for (let i = 0; i < checkerboardResult.length; i++)
+            emptyArr[i] = JSON.parse(checkerboardResult[0].post_url);
 
-        return res.json(util.makeReply(reply, true, 200, '단일 게시물 목록입니다.', { loadingResult }));
+        const checkerboardArr = [];
+        for (let i = 0; i < checkerboardResult.length; i++)
+            checkerboardArr[i] = emptyArr[0][i];
+
+        return res.json(util.dataReply(dataReply, true, 200, '바둑판 형식의 사용자 피드입니다.', { checkerboardArr }));
     } catch (err) {
         console.log(err);
 
@@ -107,6 +113,21 @@ export async function userCheckerboard(req, res) {
 }
 
 // 단일 형식으로 사용자 게시물 불러오기
-/**export async function userSingle(req, res) {
-    const 
-}*/
+export async function userSingle(req, res) {
+    const user_id = req.decoded.user_id;
+
+    try {
+        const singleResult = await mainService.importSingle(user_id);
+        
+        for (let i = 0; i < singleResult.length; i++)
+            singleResult[i].post_url = JSON.parse(singleResult[i].post_url);
+
+        console.log(singleResult);
+
+        return res.json(util.dataReply(dataReply, true, 200, '단일 게시물 형식의 사용자 피드입니다.', { singleResult }));
+    } catch (err) {
+        console.log(err);
+
+        return res.json(util.dataReply(dataReply, false, 500, 'Server error response', { err: err.message }));
+    }
+}
