8a36420952e87656fc7d1a83aa61541c37da8e8a
SangHuPark <tkdgn5408407@gmail.com>
2022-10-20 19:03:00 +0900

    remove myFeed, newest API apply

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -20,21 +20,6 @@ export async function myProfile(req, res) {
     }
 }
 
-// 내 프로필 피드 불러오기
-export async function myFeed(req, res) {
-    const user_id = req.decoded.user_id;
-
-    try {
-        const myFeedInfo = await mainService.findUserFeed(user_id);
-
-        return res.json(util.dataReply(dataReply, true, 200, '요청한 회원의 피드 정보입니다.', { myFeedInfo }));
-    } catch (err) {
-        console.log(err);
-        
-        return res.json(util.dataReply(dataReply, false, 500, 'Server error response', { err: err.message }));
-    }
-}
-
 // 프로필 사진 설정
 export async function setProfileImg(req, res, next) {
     const user_id = req.decoded.user_id;
@@ -118,7 +103,7 @@ export async function userSingle(req, res) {
     const user_id = req.decoded.user_id;
 
     try {
-        const singleResult = await mainService.importSingle(user_id);
+        const singleResult = await mainService.importUserSingle(user_id);
         
         for (let i = 0; i < singleResult.length; i++)
             singleResult[i].post_url = JSON.parse(singleResult[i].post_url);
@@ -129,4 +114,101 @@ export async function userSingle(req, res) {
 
         return res.json(util.dataReply(dataReply, false, 500, 'Server error response', { err: err.message }));
     }
+}
+
+export async function newestCheckerboard(req, res) {
+    try {
+        const checkerboardResult = await mainService.importNewestCheckerboard();
+
+        const emptyArr = [];
+        for (let i = 0; i < checkerboardResult.length; i++)
+            emptyArr[i] = JSON.parse(checkerboardResult[i].post_url);
+        
+        const checkerboardArr = [];
+        for (let i = 0; i < checkerboardResult.length; i++)
+            checkerboardArr[i] = emptyArr[i][0];
+
+        return res.json(util.dataReply(dataReply, true, 200, '바둑판 형식의 사용자 피드입니다.', { checkerboardArr }));
+    } catch (err) {
+        console.log(err);
+
+        return res.json(util.dataReply(dataReply, false, 500, 'Server error response', { err: err.message }));
+    }
+}
+
+export async function newestSingle(req, res) {
+    try {
+        const singleResult = await mainService.importNewestSingle();
+        
+        for (let i = 0; i < singleResult.length; i++)
+            singleResult[i].post_url = JSON.parse(singleResult[i].post_url);
+
+        return res.json(util.dataReply(dataReply, true, 200, '단일 게시물 형식의 사용자 피드입니다.', { singleResult }));
+    } catch (err) {
+        console.log(err);
+
+        return res.json(util.dataReply(dataReply, false, 500, 'Server error response', { err: err.message }));
+    }
+}
+
+/** 오늘의 색상 관련 API
+export async function colorCheckerboard(req, res) {
+    try {
+        const checkerboardResult = await mainService.importColorCheckerboard();
+
+        const emptyArr = [];
+        for (let i = 0; i < checkerboardResult.length; i++)
+            emptyArr[i] = JSON.parse(checkerboardResult[i].post_url);
+        
+        const checkerboardArr = [];
+        for (let i = 0; i < checkerboardResult.length; i++)
+            checkerboardArr[i] = emptyArr[i][0];
+
+        return res.json(util.dataReply(dataReply, true, 200, '바둑판 형식의 사용자 피드입니다.', { checkerboardArr }));
+    } catch (err) {
+        console.log(err);
+
+        return res.json(util.dataReply(dataReply, false, 500, 'Server error response', { err: err.message }));
+    }
+}
+
+export async function colorSingle(req, res) {
+    try {
+        const singleResult = await mainService.importNewestSingle();
+        
+        for (let i = 0; i < singleResult.length; i++)
+            singleResult[i].post_url = JSON.parse(singleResult[i].post_url);
+
+        return res.json(util.dataReply(dataReply, true, 200, '단일 게시물 형식의 사용자 피드입니다.', { singleResult }));
+    } catch (err) {
+        console.log(err);
+
+        return res.json(util.dataReply(dataReply, false, 500, 'Server error response', { err: err.message }));
+    }
+} */
+
+export async function genderCheckerboard(req, res) {
+    const user_gender = req.body.user_gender;
+
+    try {
+        const checkerboardResult = await mainService.importGenderCheckerboard(user_gender);
+
+        const emptyArr = [];
+        for (let i = 0; i < checkerboardResult.length; i++)
+            emptyArr[i] = JSON.parse(checkerboardResult[i].post_url);
+        
+        const checkerboardArr = [];
+        for (let i = 0; i < checkerboardResult.length; i++)
+            checkerboardArr[i] = emptyArr[i][0];
+
+        return res.json(util.dataReply(dataReply, true, 200, '바둑판 형식의 사용자 피드입니다.', { checkerboardArr }));
+    } catch (err) {
+        console.log(err);
+
+        return res.json(util.dataReply(dataReply, false, 500, 'Server error response', { err: err.message }));
+    }
+}
+
+export async function genderSingle(req, res) {
+    
 }
