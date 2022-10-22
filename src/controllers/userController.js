fb291a54f320fb2d463fb8dd9400399ad8ab8c46
SangHuPark <tkdgn5408407@gmail.com>
2022-10-22 20:59:23 +0900

    savePost API apply

---
+++ src/controllers/userController.js
@@ -0,0 +1,93 @@
+import * as userModel from "../model/userModel.js";
+import * as util from "../function/replyFunc.js";
+import { awsUpload } from "../function/multer.js";
+
+var reply = {};
+var dataReply = {};
+
+// 마이프로필 불러오기
+export async function myProfile(req, res) {
+    const user_id = req.decoded.user_id;
+    
+    try {
+        const myProfileInfo = await userModel.findUserProfile(user_id);
+        
+        return res.json(util.dataReply(dataReply, true, 200, '요청한 회원의 프로필 정보입니다.', { myProfileInfo }));
+    } catch (err) {
+        console.log(err);
+        
+        return res.json(util.dataReply(dataReply, false, 500, 'Server error response', { err: err.message }));
+    }
+}
+
+// 소개글 설정
+export async function setProfileIntro(req, res) {
+    const user_id = req.decoded.user_id;
+    const user_introduce = req.body.user_introduce;
+
+    try {
+        await userModel.insertProfileIntro(user_id, user_introduce);
+
+        return res.json(util.makeReply(reply, true, 200, '프로필 소개가 저장되었습니다.'));
+    } catch (err) {
+        console.log(err);
+
+        return res.json(util.dataReply(dataReply, false, 500, 'Server error response', { err: err.message }));
+    }
+}
+
+// 바둑판 형식으로 사용자 게시물 불러오기
+export async function userCheckerboard(req, res) {
+    const id = req.decoded.id;
+
+    try {
+        const checkerboardResult = await userModel.importUserCheckerboard(id);
+        
+        const emptyArr = [];
+        for (let i = 0; i < checkerboardResult.length; i++)
+            emptyArr[i] = JSON.parse(checkerboardResult[i].post_url);
+        
+        const checkerboardArr = [];
+        for (let i = 0; i < checkerboardResult.length; i++)
+            checkerboardArr[i] = emptyArr[i][0];
+
+        return res.json(util.dataReply(dataReply, true, 200, '바둑판 형식의 사용자 피드입니다.', { checkerboardArr }));
+    } catch (err) {
+        console.log(err);
+
+        return res.json(util.dataReply(dataReply, false, 500, 'Server error response', { err: err.message }));
+    }
+}
+
+// 단일 형식으로 사용자 게시물 불러오기
+export async function userSingle(req, res) {
+    const id = req.decoded.id;
+
+    try {
+        const singleResult = await userModel.importUserSingle(id);
+        
+        for (let i = 0; i < singleResult.length; i++)
+            singleResult[i].post_url = JSON.parse(singleResult[i].post_url);
+        
+        return res.json(util.dataReply(dataReply, true, 200, '단일 게시물 형식의 사용자 피드입니다.', { singleResult }));
+    } catch (err) {
+        console.log(err);
+
+        return res.json(util.dataReply(dataReply, false, 500, 'Server error response', { err: err.message }));
+    }
+}
+
+export async function savePost(req, res) {
+    const id = req.decoded.id;
+    const post_id = req.body.post_id;
+
+    try {
+        await userModel.insertSavePost(id, parseInt(post_id));
+
+        return res.json(util.makeReply(reply, true, 200, '게시글이 저장되었습니다.'));
+    } catch (err) {
+        console.log(err);
+
+        return res.json(util.dataReply(dataReply, false, 500, 'Server error response', { err: err.message }));
+    }
+}
---
+++ src/model/userModel.js
@@ -0,0 +1,95 @@
+import { prisma } from "../function/prisma-client.js";
+
+export async function findUserProfile(user_id) {
+    const profileResult = await prisma.user.findUnique({
+        where : {
+            user_id,
+        },
+        select : {
+            user_nick: true,
+            profile_url: true,
+            user_introduce: true,
+        }
+    })
+    .catch((err) => {
+        throw new Error(err);
+    });
+
+    return profileResult;
+}
+
+export async function insertProfileIntro(user_id, user_introduce) {
+    await prisma.user.update({
+        where: {
+            user_id,
+        },
+        data: {
+            user_introduce,
+        },
+    })
+    .catch((err) => {
+        throw new Error(err);
+    });
+}
+
+export async function importUserCheckerboard(id) {
+    const loadingResult = await prisma.post.findMany({
+        where: {
+            post_user: id,
+        },
+        select: {
+            post_url: true,
+        },
+        orderBy: {
+            post_id: 'desc',
+        },
+    })
+    .catch((err) => {
+        throw new Error(err);
+    });
+
+    return loadingResult;
+}
+
+export async function importUserSingle(id) {
+    const singleResult = await prisma.post.findMany({
+        where : {
+            post_user: id,
+        },
+        select : {
+            post_content: true,
+            likeCount: true,
+            user_gender: true,
+            user_tall: true,
+            user_weight: true,
+            created_at: true,
+            post_url: true,
+            post_users: {
+                select: {
+                    user_nick: true,
+                    profile_url: true,
+                },
+            },
+        },
+        orderBy : {
+            post_id: 'desc',
+        },
+    })
+    .catch((err) => {
+        throw new Error(err);
+    });
+
+    return singleResult;
+}
+
+export async function insertSavePost(id, post_id) {
+    await prisma.save_post.create({
+        data: {
+            stored_user: id,
+            stored_post: post_id,
+        },
+    })
+    .catch((err) => {
+        throw new Error(err);
+    });
+}
