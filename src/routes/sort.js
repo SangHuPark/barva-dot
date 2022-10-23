a2f5dc739864347a8312bd26348ef1936d38ddb4
SangHuPark <tkdgn5408407@gmail.com>
2022-10-23 17:25:06 +0900

    color URL apply

--- src/routes/sort.js
+++ src/routes/sort.js
@@ -10,6 +10,8 @@ export default async (app) => {
 
     route.get('/newestCheckerboard', sortController.newestCheckerboard);
     route.get('/newestSingle', sortController.newestSingle);
+    route.get('/colorCheckerboard', sortController.colorCheckerboard);
+    route.get('/colorSingle', sortController.colorSingle);
     route.post('/genderCheckerboard', sortController.genderCheckerboard);
     route.post('/genderSingle', sortController.genderSingle);
 }
